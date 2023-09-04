571be77b7bd48f1d1968f86c881b153855e4c2ae
Sunil H N <sunilh.n27@gmail.com>
2023-09-05 00:04:46 +0530

    Completed Contact Component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import ContactImg from '@/Asserts/Images/Contact.png'
+import SendImg from '@/Asserts/Images/Send.png'
 
 function Contact() {
     return (
@@ -11,34 +12,44 @@ function Contact() {
 
                 <div className='flex flex-col'>
                     <div className=''>
-                        <h1 className='text-7xl w-[37rem] h-[10rem]'>Got a project in <span className='text-[#00ADB5] font-bold'>mind? </span></h1>
-                        <div className='border-2 border-emerald-500 mx-32' >
-                            <Image src={ContactImg} alt='pic' className=''/>
+                        <h1 className='text-[3.5rem] mb-12 md:-mt-8 md:text-[4.5rem] md:w-[37rem] md:h-[10rem] 2xl:w-[37rem] 2xl:h-[10rem] 2xl:text-[5.1rem]'>Got a project in <span className='text-[#00ADB5] font-bold'>mind? </span></h1>
+                        <div className=' mx-32' >
+                            <Image src={ContactImg} alt='pic' className='' />
                         </div>
                     </div>
                 </div>
 
-                <div className='flex flex-col  mx-2'>
+                <div className='mt-12 md:-mt-12 flex flex-col mx-2'>
                     <form className='space-y-3'>
                         <div className='flex space-x-3  '>
                             <div className='flex flex-col'>
                                 <label className='mb-2'>Your Name</label>
-                                <input className='px-2 rounded-lg w-[17.5rem] py-2 bg-[#393E4680]' placeholder='Name' />
+                                <input className='px-2 rounded-lg md:w-[17.5rem] py-2 bg-[#393E4680]' placeholder='Name' />
                             </div>
 
                             <div className='flex flex-col'>
                                 <label className='mb-2'>Your Email</label>
-                                <input className='px-2 rounded-lg w-[17.5rem]  py-2 bg-[#393E4680]' placeholder='Email' />
+                                <input className='px-2 rounded-lg md:w-[17.5rem]  py-2 bg-[#393E4680]' placeholder='Email' />
                             </div>
                         </div>
                         <div className='flex flex-col '>
                             <div className='flex flex-col '>
                                 <label className='mb-2'>Your Message</label>
-                                <input className='rounded-lg px-2 bg-[#393E4680] w-[36rem] h-[16rem] ' placeholder='Message ' />
+                                <input className='rounded-lg px-2 bg-[#393E4680] md:w-[36rem] h-[16rem] ' placeholder='Message ' />
                             </div>
 
-
                         </div>
+                        <div className=''>
+                            <div className='flex items-center'>
+                                <button className='bg-[#00ADB5] text-white flex items-center justify-center px-4 py-2 rounded-lg'>
+                                    Send Message
+                                    <span className='ml-2'>
+                                        <Image  src={SendImg} className='w-[1.45rem] h-[1.5rem]' />
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+
 
 
                     </form>
--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -6,7 +6,7 @@ function RecentWork() {
     return (
         <section className='my-44 text-white w-full h-[80rem] xl:h-[40rem] bg-my_bg_image  '>
             <div className='mx-32 md:mx-16 '>
-                <div className='-mx-28 flex flex-col w-[34rem] h-[6rem]'>
+                <div className='mx-28 flex flex-col w-[34rem] h-[6rem]'>
                     <h1 className='text-[4rem]'>My recent <span className='text-[#00ADB5]'>works</span>  </h1>
                     <div className='flex items-center space-x-6'>
                         <button className='px-5 py-2 rounded-lg bg-gray-700'>All</button>
